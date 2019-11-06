a7d649f09083b0d42f755a6caab29acbe2e9e750
BIT <BIT@DESKTOP-DOGVD1T>
2019-11-06 19:28:00 +0900

    .

--- src/chapter01/TypeCast.java
+++ src/chapter01/TypeCast.java
@@ -6,14 +6,12 @@ public class TypeCast {
 		//implicity(암묵적) Type Cast
 		int i = 10;
 		long l = i;
-		//long의 범위가 더 크니까 그냥 알아서 바꿈
 		
 		System.out.println(i + ":" + l);
 		
 		//explicity(명시적) Type Cast
 		long l2 = 1234567890L;
 		int i2 = (int)l2;
-		//작은쪽으로 갈때
 		
 		System.out.println(i2+":"+l2);
 
